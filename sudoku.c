4daf6927e5f4b6d45dbd5e2b71ea4dff46adcd2e
Boyern <nathan.boyer@ensta-paris.fr>
2022-05-17 15:26:07 +0200

    sudoku creation

--- sudoku.c
+++ sudoku.c
@@ -85,26 +85,16 @@ int valide(int** T, int i, int j, int x){
 int remplir_autres(int** grid, int row, int col)
 {
     int N = 9;
-    // Check if we have reached the 8th row
-    // and 9th column (0
-    // indexed matrix) , we are
-    // returning true to avoid
-    // further backtracking
+
     if (row == N - 1 && col == N)
         return 1;
  
-    //  Check if column value  becomes 9 ,
-    //  we move to next row and
-    //  column start from 0
     if (col == N)
     {
         row++;
         col = 0;
     }
    
-    // Check if the current position
-    // of the grid already contains
-    // value >0, we iterate for next column
     if (grid[row][col] > 0)
     {
         return remplir_autres(grid, row, col + 1);
@@ -113,33 +103,16 @@ int remplir_autres(int** grid, int row, int col)
 
     for (int num = 1; num <= N; num++)
     {
-        // Check if it is safe to place
-        // the num (1-9)  in the
-        // given row ,col  ->we move to next column
         if (valide(grid, row, col, num)==1)
         {
-            /* assigning the num in the
-               current (row,col)
-               position of the grid
-               and assuming our assigned num
-               in the position
-               is correct     */
             grid[row][col] = num;
            
-            //  Checking for next possibility with next
-            //  column
             if (remplir_autres(grid, row, col + 1)==1)
                 return 1;
         }
-       
-        // Removing the assigned num ,
-        // since our assumption
-        // was wrong , and we go for next
-        // assumption with
-        // diff num value
         grid[row][col] = 0;
-
     }
+    
     return 0;
 }
 
@@ -150,11 +123,14 @@ int main () {
 
     srand(time(NULL));
 
+
     int** tab;
+
     tab = calloc(9, sizeof(int*));
     for(int i = 0;i<9;i++) {
         tab[i] = calloc(9, sizeof(int));
     }
+
 
     remplir_diag(tab);
 
